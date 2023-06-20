0186df37282ed1ac1e9fc0cfc7f9ead51f2bbab2
Eszter Barbuscia <gubiscom@yahoo.com>
2023-06-19 18:19:49 -0700

    set up html routs

--- routes/html.js
+++ routes/html.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+//return index.html
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../routes/public/index.html"));
+});
 
+//reurn notes.html
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../public/notes.html"));
+});
 
 module.exports = router;
